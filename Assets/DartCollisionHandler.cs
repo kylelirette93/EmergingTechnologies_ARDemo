35c94a77530440cd6ce5af2e8e79f9fc46d6cd55
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2026-01-13 07:55:59 -0400

    Implemented dart collision.
    Dart now sticks in place on dart board.

---
+++ Assets/DartCollisionHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class DartCollisionHandler : MonoBehaviour
+{
+    Rigidbody rb;
+    MeshCollider collider;
+    bool isStuck = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<MeshCollider>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isStuck) return;
+        if (collision.gameObject.CompareTag("Board"))
+        {
+            StickToBoard(collision);
+        }
+    }
+
+    private void StickToBoard(Collision collision)
+    {
+        isStuck = true;
+
+        rb.linearVelocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        Transform parentTransform = transform.parent;
+        rb.transform.SetParent(parentTransform);
+
+        transform.position += transform.forward * 0.05f;
+    }
+}
